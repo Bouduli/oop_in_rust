e9280ac444bd17e0768a2fd16a2f97174f0404e7
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2023-08-29 11:57:55 +0200

    Tried implementing fruit OOP in this fool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    
+}
+
+struct Fruit {
+    name : String,
+    color : Color,
+    has_wedges : bool,
+    has_core : bool, 
+    eat_the_fruit:()
+}
+
+enum Color{
+    Red,
+    Green,
+    Yellow, 
+    Orange
+
 }
