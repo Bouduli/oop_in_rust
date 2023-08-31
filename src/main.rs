48f93c9ce569954c234f583663781933942e843a
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2023-08-31 13:32:07 +0200

    Fufilled Console dreams

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,54 @@
 use std::io;
 
 fn main(){
+
+    clear();
+    
     let mut loopBool = true;
     while loopBool{
+        clearOptions('m');
+
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read your line");
-
+        
         println!("You wrote: {}", input);
 
 
     }
 }
 
+fn clear(){
+    //Some black magic shii to clear the console
+    print!("\x1B[2J\x1B[1;1H");
+
+    //Program header. 
+    println!("SMFOP - Super Magical Fruit Oop Program");
+
+    
+}
+
+fn clearOptions(option: char){
+    // Function 
+
+    //runs clear function 
+    clear();
+
+    //CONTEXT MENU ITEMS DETERMINED BY FUNCTION PARAMETER OPTION
+
+    //main menu
+    match option{
+        'M'|'m'=>println!("What do you want to do?\n- C/c: Create fruit, - R/r: Read all fruits, - U/u: Update a fruit, -D/d : Delete a fruit, -Q/q : Quit "),
+        'C'|'c'=>println!("Create Fruit\nWhat fruit did you wish to make?"),
+        'R'|'r'=>println!("Read the fruits"),
+        'U'|'u'=>println!("Update a fruit"),
+        'D'|'d' => println!("Delete a fruit"),
+        _=>println!("fuck you"),
+
+    }
+
+}
 struct Fruit {
     name : String,
     color : Color,
@@ -22,8 +57,8 @@ struct Fruit {
     has_core : bool
 }
 impl Fruit{
-    fn eat_the_fruit(&self){ö
-        "The fruit tastes nothing"
+    fn eat_the_fruit(&self)-> String{
+        "The fruit tastes nothing".to_string()
     }
 }
 
