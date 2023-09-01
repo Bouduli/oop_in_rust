3d52cec35e08995285a9de21c1e5a72946753b67
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2023-09-01 13:45:25 +0200

    Renamed a few functions - Can take input almost ok

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,85 @@ use std::io;
 fn main(){
 
     clear();
-    
-    let mut loopBool = true;
-    while loopBool{
-        clearOptions('m');
 
+    let mut loop_bool = true;
+    while loop_bool{
+        MainMenu('m');
         let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read your line");
-        
-        println!("You wrote: {}", input);
 
+
+        io::stdin().read_line(&mut input).expect("Failed to read your line");
+        let action = input.chars().nth(0).unwrap(); 
+        loop_bool = MainMenu(action);
+        
+        io::stdin().read_line(&mut input);
 
     }
+    //To make program not close as soon as instructions are done, it awaits user input before closing.
+
+    
+
 }
 
 fn clear(){
     //Some black magic shii to clear the console
     print!("\x1B[2J\x1B[1;1H");
-
-    //Program header. 
-    println!("SMFOP - Super Magical Fruit Oop Program");
-
-    
 }
 
-fn clearOptions(option: char){
+fn MainMenu(option: char) -> bool{
     // Function 
 
     //runs clear function 
     clear();
 
+    
+    //Program header. 
+    println!("SMFOP - Super Magical Fruit Oop Program");
+
     //CONTEXT MENU ITEMS DETERMINED BY FUNCTION PARAMETER OPTION
 
     //main menu
     match option{
-        'M'|'m'=>println!("What do you want to do?\n- C/c: Create fruit, - R/r: Read all fruits, - U/u: Update a fruit, -D/d : Delete a fruit, -Q/q : Quit "),
-        'C'|'c'=>println!("Create Fruit\nWhat fruit did you wish to make?"),
-        'R'|'r'=>println!("Read the fruits"),
-        'U'|'u'=>println!("Update a fruit"),
-        'D'|'d' => println!("Delete a fruit"),
-        _=>println!("fuck you"),
+        'M'|'m'=>{println!("What do you want to do?\n- C/c: Create fruit, - R/r: Read all fruits, - U/u: Update a fruit, -D/d : Delete a fruit, -Q/q : Quit "); true}
+        'C'|'c'=>create_fruit(),
+        'R'|'r'=>read_fruits(),
+        'U'|'u'=>update_fruits(),
+        'D'|'d' =>delete_fruit(),
+        'Q'|'q' => {println!("Goodbye"); false}
+        _=>{println!("invalid inputs"); true}
 
     }
 
 }
+
+
+//Callback functions for CRUD related actions, Returns bool wether or not to continue the main loop; 
+fn create_fruit() -> bool{
+    println!("Create Fruit\n------------\nWhat is the name of your fruit?");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Your fruit name was invalid");
+    
+    println!("What color has your fruit\n--------------\n0 - Red, 1 - Green, 2 - ");
+
+    true
+}
+fn read_fruits()-> bool{
+    println!("Read the fruits");
+    true
+}
+fn update_fruits()-> bool{
+    println!("Update a fruit");
+    true
+}
+fn delete_fruit()-> bool{
+    println!("Delete a fruit");
+    true
+}
+
+
+
+
+//Fruit Related Shennanagains (?)
 struct Fruit {
     name : String,
     color : Color,
@@ -61,7 +94,6 @@ impl Fruit{
         "The fruit tastes nothing".to_string()
     }
 }
-
 enum Color{
     Red,
     Green,
